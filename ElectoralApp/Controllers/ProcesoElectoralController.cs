0e2d23ea5524c1a555343ce797205e516f71d42c
Josue Jimenez <josuejjimenezd@gmail.com>
2020-03-31 18:20:51 -0400

    Add maintenance ProcesoElectoral, add validations for Ciudadanos in ProcesoElectoral

--- ElectoralApp/Controllers/ProcesoElectoralController.cs
+++ ElectoralApp/Controllers/ProcesoElectoralController.cs
@@ -2,27 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectoralApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElectoralApp.Controllers
 {
     public class ProcesoElectoralController : Controller
     {
         private readonly SignInManager<IdentityUser> signInManager;
+        private readonly BDelectoralContext _context;
 
-        public ProcesoElectoralController(SignInManager<IdentityUser> signInManager)
+        public ProcesoElectoralController(SignInManager<IdentityUser> signInManager, BDelectoralContext context)
         {
             this.signInManager = signInManager;
+            this._context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             if (signInManager.IsSignedIn(User))
-                return RedirectToAction("Index","Admin"); 
+                return RedirectToAction("Index", "Admin");
 
-                return View();
+            return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(Ciudadanos model)
+        {
+            var ciudadano = _context.Ciudadanos
+                .Where(a => a.DocumentoDeIdentidad == model.DocumentoDeIdentidad)
+                .FirstOrDefault();
+
+            if (ciudadano == null)
+            {
+                ModelState.AddModelError("", "La cedula ingresada no existe");
+                return View(model);
+            }
+
+            if (!_context.Elecciones.Where(a => a.Estado == true).Any())
+            {
+                ModelState.AddModelError("", "No hay ningun Proceso Electoral Activo");
+            }
+
+            if (ciudadano.Estado == false)
+            {
+                ModelState.AddModelError("", "Este ciudadano se encuentra inactivo");
+            }
+
+            var eleccionEnCurso = _context
+                .Elecciones
+                .Where(a => a.Estado == true).FirstOrDefault();
+
+
+            //if((eleccionEnCurso != null) && )
+
+            /*var query = _context.Ciudadanos.FromSql(@"select c.Id from Ciudadanos as c
+                                            inner join Resultados
+                                            on Resultados.Ciudadanos_FK = c.Id
+                                            inner join Elecciones as e
+                                            on Resultados.Elecciones_FK = e.Id
+                                            where e.Estado = 'true'");*/
+
+
+
+            return View(model);
+        }
     }
 }
